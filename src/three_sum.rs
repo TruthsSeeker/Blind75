ecd972d08242a880e22f61f95996e9770f6a0d1a
TruthsSeeker <heinrichloic@gmail.com>
2024-02-08 14:45:25 +0100

    3sum Two Pointers approach

--- src/three_sum.rs
+++ src/three_sum.rs
@@ -1,5 +1,48 @@
 use std::collections::{HashMap, HashSet};
 
+pub fn three_sum_two_pointers(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    if nums.len() < 3 { return vec![]; }
+    
+    let mut result = vec![];
+    let mut sorted_nums = nums.clone();
+    sorted_nums.sort();
+
+    
+    for i in 0..sorted_nums.len() {
+        if sorted_nums[i] > 0 { return result; }
+        if i > 0 && sorted_nums[i] == sorted_nums[i - 1] { continue; }
+
+
+        let mut low = i + 1;
+        let mut high = sorted_nums.len() - 1;
+
+        while low < high {
+            let sum = sorted_nums[i] + sorted_nums[low] + sorted_nums[high];
+            if sum > 0 {
+                high -= 1;
+            } else if sum < 0 {
+                low += 1;
+            } else {
+                result.push(vec![sorted_nums[i], sorted_nums[low], sorted_nums[high]]);
+
+                let last_low = sorted_nums[low];
+                let last_high = sorted_nums[high];
+
+                while low < high && sorted_nums[low] == last_low {
+                    low += 1;
+                }
+                while low < high && sorted_nums[high] == last_high {
+                    high -= 1;
+                }
+
+            }
+        }
+
+    }
+
+    result
+}
+
 
 pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
     let mut result: Vec<Vec<i32>> = vec![];
@@ -81,5 +124,20 @@ mod tests {
         assert_eq!(left, vec![]);
 
 
+    }
+
+    #[test]
+    fn test_three_sum_two_pointers() {
+        let mut left:Vec<i32> = three_sum_two_pointers(vec![-1, 0, 1, 2, -1, -4]).into_iter().flatten().collect();
+        left.sort();
+        assert_eq!(left, vec![-1, -1, -1, 0, 1, 2]);
+
+        left = three_sum_two_pointers(vec![0, 0, 0]).into_iter().flatten().collect();
+        left.sort();
+        assert_eq!(left, vec![0, 0, 0]);
+
+        left = three_sum_two_pointers(vec![0, 1, 1]).into_iter().flatten().collect();
+        left.sort();
+        assert_eq!(left, vec![]);
     }
 }
