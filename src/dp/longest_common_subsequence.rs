b35f86d198e482678fcd41f1ba5e43093a07fda0
Loic Heinrich <heinrichloic@gmail.com>
2024-02-20 10:33:02 +0100

    LCS tab

--- src/dp/longest_common_subsequence.rs
+++ src/dp/longest_common_subsequence.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 
 pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
-        let mut memo = HashMap::new();
-        find_subsequence_memo(text1.as_str(), text2.as_str(), &mut memo)
+        // let mut memo = HashMap::new();
+        // find_subsequence_memo(text1.as_str(), text2.as_str(), &mut memo)
+        find_subsequence_tab(text1, text2)
 }
 
 fn find_subsequence_memo(text1: &str, text2: &str, memo: &mut HashMap<(String, String), i32>) -> i32 {
@@ -21,6 +22,22 @@ fn find_subsequence_memo(text1: &str, text2: &str, memo: &mut HashMap<(String, S
     }
     
     *memo.get(&(text1.to_string(), text2.to_string())).unwrap_or(&0)
+}
+
+fn find_subsequence_tab(text1: String, text2: String) -> i32 {
+    let mut table = vec![vec![0; text2.chars().count() + 1]; text1.chars().count() + 1];
+
+    for (i, c1) in text1.char_indices() {
+        for (j, c2) in text2.char_indices() {
+            if c1 == c2 {
+                // Look to upper left to look at both previous indices
+                table[i+1][j+1] = table[i][j] + 1;
+            } else {
+                table[i+1][j+1] = table[i+1][j].max(table[i][j+1]);
+            }
+        }
+    }
+    table[text1.chars().count()][text2.chars().count()]
 }
 
 #[cfg(test)]
