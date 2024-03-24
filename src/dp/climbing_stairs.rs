7469956792796b0fcb09aa43fe841b6237c280f9
Loic Heinrich <heinrichloic@gmail.com>
2024-03-24 11:56:11 +0100

    Added documentation and asymptotic analysis

--- src/dp/climbing_stairs.rs
+++ src/dp/climbing_stairs.rs
@@ -1,5 +1,28 @@
+
 use std::collections::HashMap;
 
+/// Recursive solution to leetcode 70. Climbing Stairs
+/// 
+/// link: https://leetcode.com/problems/climbing-stairs/description/
+/// 
+/// ## Complexity
+/// ### Time
+/// This is the classic case of recursive complexity. Which is branches^depth
+/// 
+/// Here branches = 2, one branch for climbing 1 step at a time, one branch for climbing 2
+/// 
+/// **O(2^n)**
+/// 
+/// Using memoization allows for deduplication of work so each problem will be solved only once.
+/// 
+/// **O(n)**
+/// 
+/// ### Space
+/// 
+/// As this algorithm is essentially a DFS of the height of the stairs to climb, 
+/// the space complexity will be equal to the depth of the graph in this case:
+/// 
+/// **O(n)**
 pub fn climb_stairs(n: i32) -> i32 {
     let mut memo: HashMap<i32, i32> = HashMap::new();
     climb(n, &mut memo)
@@ -14,7 +37,7 @@ pub fn climb(n: i32, memo: &mut HashMap<i32, i32>) -> i32 {
     } else if let Some(m) = memo.get(&n) {
         *m
     } else {
-        let result = climb(n - 1, memo) + climb(n -2, memo);
+        let result = climb(n - 1, memo) + climb(n - 2, memo);
         memo.insert(n, result);
         result
     }
